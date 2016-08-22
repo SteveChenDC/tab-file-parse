818fd03263076cfb6335c79e5a6d5f80ba294ef2
SteveC <stevechenweb@gmail.com>
2016-08-22 02:32:44 -0400

    Add ActionView::Helpers::NumberHelper library to order controller. Edit order controller to user number_to_currency helper

--- ls-challenge-app/app/controllers/orders_controller.rb
+++ ls-challenge-app/app/controllers/orders_controller.rb
@@ -1,9 +1,11 @@
 class OrdersController < ApplicationController
+  include ActionView::Helpers::NumberHelper
+
   def index
   end
-
+  
   def import
     @revenue = Order.import(params[:file])
-    redirect_to orders_path, notice: "Orders Added Successfully. Your total revenue is $#{@revenue}"
+    redirect_to orders_path, notice: "Orders Added Successfully. Your total revenue is #{number_to_currency(@revenue)}"
   end
 end
