54af4f08253366a8682246ceb48461d472e4b4f5
SteveC <stevechenweb@gmail.com>
2016-08-21 21:47:19 -0400

    Add controller input method to handle post requests. Add import method to model that creates orders and calculates the total revenue. Add form.

--- ls-challenge-app/app/controllers/orders_controller.rb
+++ ls-challenge-app/app/controllers/orders_controller.rb
@@ -1,5 +1,9 @@
 class OrdersController < ApplicationController
   def index
-    
+  end
+
+  def import
+    @revenue = Order.import(params[:file])
+    redirect_to orders_path, notice: "Orders Added Successfully. Your total revenue is $#{@revenue}"
   end
 end
--- ls-challenge-app/app/models/order.rb
+++ ls-challenge-app/app/models/order.rb
@@ -1,2 +1,12 @@
 class Order < ActiveRecord::Base
+  def self.import(file)
+    # Empty array to store the total revenue of each row
+    orders_arr = []
+    CSV.foreach(file.path, headers: true, col_sep: "\t") do |row|
+      transformed_row = row.to_hash.transform_keys!{ |key| key.to_s.gsub(" ","_") }
+      orders_arr << transformed_row["item_price"].to_f * transformed_row["purchase_count"].to_i
+      Order.create! transformed_row
+    end
+    orders_arr.inject(0, :+).to_s
+  end
 end
