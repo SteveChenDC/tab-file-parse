fb8089861fb107a05d16214ab16152288ebd9a0b
SteveC <stevechenweb@gmail.com>
2016-08-22 13:53:52 -0400

    Refactor order.rb. Edit index view

--- ls-challenge-app/app/controllers/orders_controller.rb
+++ ls-challenge-app/app/controllers/orders_controller.rb
@@ -3,7 +3,6 @@ class OrdersController < ApplicationController
 
   def index
   end
-  
   def import
     @revenue = Order.import(params[:file])
     redirect_to orders_path, notice: "Orders Added Successfully. Your total revenue is #{number_to_currency(@revenue)}"
